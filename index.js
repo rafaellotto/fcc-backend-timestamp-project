17cc549a70301e58dc6387dbcfb5af0f1ccbae36
Rafael Lotto Machado <ralomach@gmail.com>
2022-05-14 15:15:54 -0300

    Project finished

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +18,32 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let date = new Date();
+
+  if (req.params.date !== undefined) {
+    let timestamp = Date.parse(req.params.date);
+
+    if (isNaN(timestamp)) {
+      timestamp = parseInt(req.params.date);
+
+      if (isNaN(timestamp)) {
+        return res.json({ "error": "Invalid date" });
+      }
+    }
+
+    date = new Date(timestamp);
+  }
+
+  res.json({
+    "unix": date.getTime(),
+    "utc": date.toUTCString(),
+  });
+
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(3000, function () {
+  console.log('Your app is listening on http://localhost:3000');
 });
